351c96e96e11ab7af43f558e1429d55fd0fb4108
John-Thailand <hamoban69@gmail.com>
2022-01-22 18:03:50 +0900

    add drawer menu

--- lib/view/side_menu/side_menu_page.dart
+++ lib/view/side_menu/side_menu_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_pwa/util/constants.dart';
 import 'package:todo_pwa/view/style.dart';
+import 'package:todo_pwa/view_model/view_model.dart';
 
 class SideMenuPage extends StatelessWidget {
   const SideMenuPage({Key? key}) : super(key: key);
@@ -8,6 +11,37 @@ class SideMenuPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: PageColor.sideMenuBgColor,
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            child: Column(
+              children: [
+                FlutterLogo(
+                  size: 100.0,
+                ),
+                Text(StringR.appTitle),
+              ],
+            ),
+          ),
+          ListTile(
+            title: Text(StringR.addNewTask),
+            onTap: () {
+              final viewModel = context.read<ViewModel>();
+              final screenSize = viewModel.screenSize;
+              if (screenSize != ScreenSize.LARGE) Navigator.pop(context);
+              _addNewTask(context);
+            },
+          ),
+          SwitchListTile(
+            title: Text(StringR.isFinishedTaskIncluded),
+            value: false,
+            onChanged: null,
+          ),
+        ],
+      ),
     );
   }
+
+  _addNewTask(BuildContext context) {}
 }
--- lib/view/task_list/task_list_page.dart
+++ lib/view/task_list/task_list_page.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_pwa/util/constants.dart';
+import 'package:todo_pwa/view/side_menu/side_menu_page.dart';
 import 'package:todo_pwa/view/style.dart';
+import 'package:todo_pwa/view_model/view_model.dart';
 
 class TaskListPage extends StatelessWidget {
   const TaskListPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: PageColor.taskListBgColor,
-    );
+    return Consumer<ViewModel>(builder: (context, vm, child) {
+      final screenSize = vm.screenSize;
+
+      return Scaffold(
+        backgroundColor: PageColor.taskListBgColor,
+        appBar: AppBar(
+          title: Text(StringR.taskList),
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () => _sort(context),
+              icon: const Icon(Icons.sort),
+            ),
+          ],
+        ),
+        floatingActionButton: (screenSize == ScreenSize.LARGE)
+            ? null
+            : FloatingActionButton(
+                onPressed: () => _addNewTask(context),
+                child: const Icon(Icons.add),
+              ),
+        drawer: (screenSize != ScreenSize.LARGE)
+            ? Drawer(child: SideMenuPage())
+            : null,
+      );
+    });
   }
+
+  _sort(BuildContext context) {}
+
+  _addNewTask(BuildContext context) {}
 }
