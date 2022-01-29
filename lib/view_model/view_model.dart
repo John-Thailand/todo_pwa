3808d2bddb82b449df6b609278feaa9d3a1d6577
John-Thailand <hamoban69@gmail.com>
2022-01-29 14:43:22 +0900

    コメント追記

--- lib/view_model/view_model.dart
+++ lib/view_model/view_model.dart
@@ -8,12 +8,17 @@ class ViewModel extends ChangeNotifier {
 
   ViewModel({required this.repository});
 
+  // スクリーンサイズ
   ScreenSize screenSize = ScreenSize.SMALL;
 
+  // 選択されたタスクのリスト
   List<Task> selectedTaskList = [];
+  // ソートされているか
   bool isSorted = false;
+  // 完了したタスクも含むのか
   bool isFinishedTasksIncluded = false;
 
+  // 新しいタスクを追加
   void addNewTask(
     String title,
     String detail,
@@ -29,6 +34,7 @@ class ViewModel extends ChangeNotifier {
     getTaskList();
   }
 
+  // タスクリストの取得
   void getTaskList() {
     selectedTaskList = repository.getTaskList(
       isSorted,
@@ -36,26 +42,31 @@ class ViewModel extends ChangeNotifier {
     );
   }
 
+  // ソート
   void sort(bool isSort) {
     isSorted = isSort;
     getTaskList();
   }
 
+  // タスクを完了する
   void finishTask(Task selectedTask, isFinished) {
     repository.finishTask(selectedTask, isFinished);
     getTaskList();
   }
 
+  // タスクを削除する
   void deleteTask(Task selectedTask) {
     repository.deleteTask(selectedTask);
     getTaskList();
   }
 
+  // UNDO処理
   undo() {
     repository.undo();
     getTaskList();
   }
 
+  // 完了ステータスに変更
   changeFinishStatus(bool isIncluded) {
     isFinishedTasksIncluded = isIncluded;
     getTaskList();
